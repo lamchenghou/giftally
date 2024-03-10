ca5709e9d106025f5237ab151a0bb6468f2f39d9
lamchenghou <lamchenghouu@gmail.com>
2024-03-10 19:19:20 +0800

    Expose more backend endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,35 +7,113 @@ const app = express();
 app.use(express.json());
 
 // CORS Middleware
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
-app.get("/teams", async (req, res) => {
+app.get("/staff", async (req, res) => {
+  const searchQueryParam = req.query.search as string;
+  if (searchQueryParam) {
+    const narrowedStaff = await prisma.staff.findMany({
+      where: {
+        staffPassId: {
+          contains: searchQueryParam,
+          mode: "insensitive",
+        },
+      },
+    });
+    res.json(narrowedStaff);
+  } else {
+    const staff = await prisma.staff.findMany();
+    res.json(staff);
+  }
+});
+
+app.get("/teams", async (_req, res) => {
   const teams = await prisma.team.findMany();
   res.json(teams);
+});
+
+// Query redemption history
+app.get("/teams/redemption-hist", async (req, res) => {
+  const count = Number(req.query.count);
+  const offset = Number(req.query.offset);
+
+  const teams = await prisma.team.findMany({
+    where: {
+      hasRedeemed: true,
+    },
+    orderBy: {
+      redeemedAt: "desc",
+    },
+    skip: offset,
+    take: count,
+  });
+
+  res.json(teams);
+});
+
+app.get("/teams/:teamname", async (req, res) => {
+  const { teamname } = req.params;
+
+  const team = await prisma.team.findFirst({
+    where: {
+      teamName: teamname,
+    },
+  });
+
+  if (team === null) {
+    res.status(404).send("Team not found.");
+  }
+
+  res.json(team);
+});
+
+app.put("/teams/:teamname/redeem", async (req, res) => {
+  // Request body
+  const body = req.body;
+  // URL params
+  const { teamname } = req.params;
+  // Get the team to do perform a check on whether they can redeem
+  const initialTeamData = await prisma.team.findFirst({
+    where: { teamName: teamname },
+    select: { hasRedeemed: true },
+  });
+
+  if (initialTeamData === null) {
+    res.status(404).send("Team not found.");
+  }
+
+  if (initialTeamData?.hasRedeemed !== false) {
+    res
+      .status(400)
+      .send(
+        `Team has already redeemed. hasRedeemed: ${JSON.stringify(
+          initialTeamData
+        )}`
+      );
+  } else {
+    const updatedTeamData = await prisma.team.update({
+      where: {
+        teamName: teamname,
+      },
+      data: {
+        hasRedeemed: true,
+        redeemedAt: String(Date.now()),
+        collectorId: body.collectorId,
+      },
+    });
+
+    res.json(updatedTeamData);
+  }
 });
 
 const server = app.listen(3000, () =>
   console.log("Server listening at port 3000")
 );
-// async function main() {
-//   // ... you will write your Prisma Client queries here
-//   const allTeams = await prisma.team.findMany();
-//   console.log(allTeams);
-// }
-
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
